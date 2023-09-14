94e9665bce5aca65e07873c779aa82255f7007eb
Ike <ikelimjl@gmail.com>
2023-09-14 17:41:59 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="grid min-h-screen w-full place-items-center">
+      <h1>This site is still under development. Please try again later.</h1>
+    </main>
+  );
+}
