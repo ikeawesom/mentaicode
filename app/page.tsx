8a7a08662318134b4bcb0caac98b5ea5663892e5
Ike <ikelimjl@gmail.com>
2023-09-22 02:13:28 +0800

    basic pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Hero, { HeroButtons } from "@/components/Hero";
+import LandingPage from "@/components/LandingPage";
+
 export default function Home() {
   return (
-    <main className="grid min-h-screen w-full place-items-center">
-      <h1>This site is still under development. Please try again later.</h1>
-    </main>
+    <div>
+      <LandingPage>
+        <Hero
+          header="Empower Your Digital Journey with Innovative Solutions."
+          subtext="Hi, I'm Ike, a junior developer passionate about creating digital magic. Explore my work, discover templates and courses, and let's craft something incredible together."
+        >
+          <HeroButtons />
+        </Hero>
+      </LandingPage>
+    </div>
   );
 }
