84476cb9e895606e7675324d6cc31f8803f32190
Ike <ikelimjl@gmail.com>
2023-09-22 02:11:59 +0800

    added development screen

---
+++ components/Developing.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Developing() {
+  return (
+    <main className="grid min-h-screen w-full place-items-center">
+      <h1>This site is still under development. Please try again later.</h1>
+    </main>
+  );
+}
