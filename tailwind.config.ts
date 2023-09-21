c81d9818550cc7674fc478d0a91a510837ae37d8
Ike <ikelimjl@gmail.com>
2023-09-21 22:18:12 +0800

    initalize styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'font-default': 'var(--font-default)',
+        'font-warp': 'var(--font-warp)',
+      },
+      colors: {
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'border': 'var(--border)',
+      
+        'custom-black': 'var(--custom-black)',
+        'custom-white': 'var(--custom-white)',
+        'dark-grey': 'var(--dark-grey)',
+        'light-grey': 'var(--light-grey)',
+      }
     },
   },
   plugins: [],
