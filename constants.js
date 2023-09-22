c388e2101ff1f844572d5e95ec08b94d925442fc
Ike <ikelimjl@gmail.com>
2023-09-22 22:31:32 +0800

    constant links

--- constants.js
+++ constants.js
@@ -1,2 +1,11 @@
 export const INITIAL_ANIM_TIME = 0.8;
 export const ANIM_DELAY_FACTOR = 0.2;
+export const LOGO_WIDTH = 30;
+export const LOGO_WORD_WIDTH = 150;
+
+export const NAVIGATION_LINKS = [
+  { title: "About", link: "/about" },
+  { title: "Portfolio", link: "/portfolio" },
+  { title: "Shop", link: "/shop" },
+  { title: "Blog", link: "/blog" },
+];
