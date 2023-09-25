53a53188ec1a4e7194ee88d528c7ce038b5ce96c
Ike <ikelimjl@gmail.com>
2023-09-25 21:23:17 +0800

    edited page wrapper

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -9,7 +9,7 @@ type PageWrapperProps = {
 export default function PageWrapper({ children }: PageWrapperProps) {
   return (
     <div
-      className={`p-8 bg-gradient-to-b min-h-screen ${
+      className={`p-8 px-48 bg-gradient-to-b min-h-screen ${
         LightCheck()
           ? "from-custom-white to-border"
           : "from-custom-black to-custom-black"
