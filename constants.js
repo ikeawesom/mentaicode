b5c1e0c3fbcfc2257c856687030a3b181f1fa990
Ike <ikelimjl@gmail.com>
2023-09-26 22:22:13 +0800

    animations

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
-export const INITIAL_ANIM_TIME = 0.8;
+export const INITIAL_ANIM_TIME = 1;
 export const ANIM_DELAY_FACTOR = 0.2;
-export const LOGO_WIDTH = 30;
+export const LOGO_WIDTH = 40;
 export const LOGO_WORD_WIDTH = 150;
 
 export const NAVIGATION_LINKS = [
