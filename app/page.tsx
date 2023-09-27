9808d38872a6d2be8570840f1ec3a38bd18300bc
Ike <ikelimjl@gmail.com>
2023-09-27 22:07:09 +0800

    file organisation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero, { HeroButtons } from "@/components/Hero";
-import LandingPage from "@/components/sections/LandingPage";
-import TechnologiesSection from "@/components/sections/Technologies";
+import { LandingPage, TechnologiesSection } from "@/screens/home";
 
 export default function Home() {
   return (
