bff1278070a7fbdaa6577d0447ff974aa1474def
Ike <ikelimjl@gmail.com>
2023-09-27 22:06:57 +0800

    file organisation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,9 +3,11 @@ import React from "react";
 import { LightCheck } from "@/contexts/ThemeContext";
 import { motion } from "framer-motion";
 import { ANIM_DELAY_FACTOR, INITIAL_ANIM_TIME } from "@/constants.js";
-import GetStartedButton from "./buttons/GetStartedButton";
-import SecondaryButton from "./buttons/SecondaryButton";
+import GetStartedButton from "./utilities/buttons/GetStartedButton";
+import SecondaryButton from "./utilities/buttons/SecondaryButton";
 import DivFadeUp from "./animations/DivFadeUp";
+import MainHeader from "./utilities/headers/MainHeader";
+import { ParaFadeUp } from "./animations";
 
 type HeroType = {
   header: string;
@@ -17,32 +19,17 @@ export default function Hero({ header, subtext, children }: HeroType) {
   return (
     <div className="flex flex-col gap-8 items-center justify-center w-full">
       <DivFadeUp className="flex flex-col gap-4 text-center w-full justify-center items-center">
-        <h1
-          className={`${
-            LightCheck() ? "header-light drop-shadow-md" : "header-dark"
-          } lg:w-[860px] md:w-[620px] sm:w-[500px] max-[639px]:text-5xl max-[320px]:text-4xl`}
-        >
+        <MainHeader headerLight="header-light" headerDark="header-dark">
           {header}
-        </h1>
-        <motion.p
-          initial={{ opacity: 0, y: -10 }}
-          whileInView={{
-            opacity: 1,
-            y: 0,
-            transition: {
-              duration: INITIAL_ANIM_TIME,
-              ease: "easeInOut",
-              delay: ANIM_DELAY_FACTOR,
-            },
-          }}
-          viewport={{ once: true }}
+        </MainHeader>
+        <ParaFadeUp
           className={
             "lg:w-[700px] md:w-[540px] sm:w-[450px] max-[499px]:text-md max-[320px]:text-sm font-light subtext " +
             (LightCheck() ? "text-custom-black" : "text-custom-white")
           }
         >
           {subtext}
-        </motion.p>
+        </ParaFadeUp>
       </DivFadeUp>
       {children}
     </div>
