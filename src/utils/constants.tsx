9e4ca346bbf6f1a6bc1fbe7c9ef6c36de93eaa9c
Ike <ikelimjl@gmail.com>
2023-09-29 00:21:22 +0800

    file organization

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -2,6 +2,7 @@ export const INITIAL_ANIM_TIME = 0.8;
 export const ANIM_DELAY_FACTOR = 0.2;
 export const LOGO_WIDTH = 40;
 export const LOGO_WORD_WIDTH = 150;
+export const ICONS_SIZE = 80;
 
 export const NAVIGATION_LINKS = [
   { title: "About", link: "/about" },
@@ -17,4 +18,21 @@ export const TECH_LIST = [
   { title: "Supabase", icon: "icon_supabase.svg" },
   { title: "Python", icon: "icon_python.svg" },
   { title: "Git", icon: "icon_git.svg" },
+];
+
+export const TESTIMONIALS = [
+  {
+    name: "Teo Qing Ren",
+    from: "Anglo-Chinese Junior College",
+    src: "teo.png",
+    quote:
+      "Ike is a confident young man who has displayed a strong sense of purpose in all that he pursues. He is undaunted by challenges and views them as learning opportunities to improve himself.",
+  },
+  {
+    name: "Britmand Wong",
+    from: "Ngee Ann Secondary School",
+    src: "wong.jfif",
+    quote:
+      "Meticulous and thorough in nature, Ike takes pride in the quality of the work he presents and works steadily towards his goals.",
+  },
 ];
