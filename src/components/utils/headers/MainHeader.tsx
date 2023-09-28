9272f0ebead10af2b089f0b6efd2e33520ea0156
Ike <ikelimjl@gmail.com>
2023-09-29 00:19:52 +0800

    customized header styles

--- src/components/utils/headers/MainHeader.tsx
+++ src/components/utils/headers/MainHeader.tsx
@@ -1,20 +1,13 @@
+"use client";
 import { LightCheck } from "@/src/contexts/ThemeContext";
 import React from "react";
 
 type HeaderType = {
   className?: string;
   children?: React.ReactNode;
-  headerLight:
-    | "header-light"
-    | "header-light-1"
-    | "header-light-2"
-    | "header-light-3";
+  headerLight?: "header-light" | "header-1" | "header-2" | "header-3";
 
-  headerDark:
-    | "header-dark"
-    | "header-dark-1"
-    | "header-dark-2"
-    | "header-dark-3";
+  headerDark?: "header-dark" | "header-1" | "header-2" | "header-3";
 };
 
 export default function MainHeader({
@@ -27,7 +20,7 @@ export default function MainHeader({
     <h1
       className={`${
         LightCheck() ? `${headerLight} drop-shadow-md` : headerDark
-      } lg:w-[1000px] md:w-[620px] sm:w-[500px] max-[639px]:text-5xl max-[320px]:text-4xl ${className} text-center`}
+      } min-[1380px]:w-[1000px] lg:w-[800px] md:w-[620px] sm:w-[500px] max-[639px]:text-5xl max-[320px]:text-4xl ${className} text-center`}
     >
       {children}
     </h1>
