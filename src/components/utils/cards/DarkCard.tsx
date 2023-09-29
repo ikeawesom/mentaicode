592048b766dc6aeceb3f4484d772e6f489c187f1
Ike <ikelimjl@gmail.com>
2023-09-29 19:56:20 +0800

    modified styling

--- src/components/utils/cards/DarkCard.tsx
+++ src/components/utils/cards/DarkCard.tsx
@@ -7,7 +7,7 @@ type CardTypes = {
 export default function DarkCard({ className, children }: CardTypes) {
   return (
     <div
-      className={`border-transparent border-2 min-[330px]:dark:border-slate-800 min-[330px]:dark:bg-gradient-to-br min-[330px]:dark:from-slate-800 min-[330px]:dark:to-custom-black rounded-md min-[330px]:p-5 ${className}`}
+      className={`border-transparent border-[1.5px] min-[330px]:dark:border-slate-700 min-[330px]:dark:bg-gradient-to-br min-[330px]:dark:from-slate-800/50 min-[330px]:dark:to-custom-black/80 rounded-md min-[630px]:p-8 min-[330px]:p-5 min-[330px]:dark:backdrop-blur-2xl ${className}`}
     >
       {children}
     </div>
