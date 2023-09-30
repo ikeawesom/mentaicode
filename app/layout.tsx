8339b8d7220098968a40df36dce1343838ab2768
Ike <ikelimjl@gmail.com>
2023-10-01 01:39:59 +0800

    localized page wrapper to each page

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         ) : (
           <body>
             <Navbar />
-            <PageWrapper>{children}</PageWrapper>
+            {children}
             <Footer />
           </body>
         )}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import PageWrapper from "@/src/components/PageWrapper";
 import AnimationWrapper from "@/src/components/animations/AnimationWrapper";
 import Hero, { HeroButtons } from "@/src/components/sections/Hero";
 import {
@@ -10,19 +11,21 @@ import {
 
 export default function Home() {
   return (
-    <AnimationWrapper>
-      <LandingPage>
-        <Hero
-          header="Empower Your Digital Journey with Innovative Solutions."
-          subtext="Hi, I'm Ike, a skilled full-stack developer dedicated to turning ideas into innovative software applications. Explore my latest projects and articles, discover templates and courses, and let's craft something incredible together."
-        >
-          <HeroButtons />
-        </Hero>
-      </LandingPage>
-      <TechnologiesSection />
-      <TestimonialsSection />
-      <ProjectSection />
-      <ActionCallSection />
-    </AnimationWrapper>
+    <PageWrapper>
+      <AnimationWrapper>
+        <LandingPage>
+          <Hero
+            header="Empower Your Digital Journey with Innovative Solutions."
+            subtext="Hi, I'm Ike, a skilled full-stack developer dedicated to turning ideas into innovative software applications. Explore my latest projects and articles, discover templates and courses, and let's craft something incredible together."
+          >
+            <HeroButtons />
+          </Hero>
+        </LandingPage>
+        <TechnologiesSection />
+        <TestimonialsSection />
+        <ProjectSection />
+        <ActionCallSection />
+      </AnimationWrapper>
+    </PageWrapper>
   );
 }
