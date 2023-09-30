dcd6b3c055104875f8d6aaf7bad9e1953d32f130
Ike <ikelimjl@gmail.com>
2023-10-01 01:41:00 +0800

    removed AnimatePresence

--- src/components/animations/AnimationWrapper.tsx
+++ src/components/animations/AnimationWrapper.tsx
@@ -5,15 +5,16 @@ import Splashscreen from "./Splashscreen";
 
 type WrapperType = {
   children?: React.ReactNode;
+  className?: string;
 };
 
-export default function AnimationWrapper({ children }: WrapperType) {
+export default function AnimationWrapper({ children, className }: WrapperType) {
   const pathname = usePathname();
   console.log(pathname);
   return (
-    <>
+    <div className={className}>
       <Splashscreen />
       {children}
-    </>
+    </div>
   );
 }
