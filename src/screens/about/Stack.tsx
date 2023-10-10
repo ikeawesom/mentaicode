2770e79490f6f0dfb5b9365f9416d1cc7b343f5c
ikeawesom <ikelimjl@gmail.com>
2023-10-10 23:04:00 +0800

    added header for stack

--- src/screens/about/Stack.tsx
+++ src/screens/about/Stack.tsx
@@ -5,11 +5,26 @@ import { ICONS_SIZE, TECH_CAT, TECH_LIST } from "@/src/utils/constants";
 import Image from "next/image";
 import DarkCard from "@/src/components/utils/cards/DarkCard";
 import GlowBlur from "@/src/components/sections/GlowBlur";
-import { LiFadeIn } from "@/src/components/animations";
+import { DivFadeUp, LiFadeIn, ParaFadeUp } from "@/src/components/animations";
+import MainHeader from "@/src/components/utils/headers/MainHeader";
+import { LightCheck } from "@/src/contexts/ThemeContext";
 
 export default function Stack() {
   return (
-    <SectionContainer className="min-[420px]:py-10 py-0">
+    <SectionContainer className="min-[420px]:py-10 py-0 flex flex-col gap-10">
+      <DivFadeUp className="flex flex-col gap-3 items-center justify-center">
+        <MainHeader headerDark="header-1" headerLight="header-1">
+          My Stack
+        </MainHeader>
+        <ParaFadeUp
+          className={`text-center font-light
+              ${LightCheck() ? "text-custom-black" : "text-custom-white"}`}
+        >
+          With a variety of advanced technologies, I create amazing web and
+          native apps using efficient and effective solutions to turn digital
+          dreams into reality.
+        </ParaFadeUp>
+      </DivFadeUp>
       <ul className="flex gap-10 items-stretch justify-stretch flex-wrap">
         {TECH_CAT.map((cat, catindex) => (
           <DarkCard
