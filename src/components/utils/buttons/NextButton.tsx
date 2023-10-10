ecd12dc9a55f72b3b6b313cee9815e39725b87d7
ikeawesom <ikelimjl@gmail.com>
2023-10-11 00:17:01 +0800

    modified styles

--- src/components/utils/buttons/NextButton.tsx
+++ src/components/utils/buttons/NextButton.tsx
@@ -4,13 +4,20 @@ import React, { useState } from "react";
 import { DivFadeUp } from "../../animations";
 import { useLottie } from "lottie-react";
 import ArrowRightPrimary from "@/public/assets/images/lottie/arrow-right-primary.json";
+import { twMerge } from "tailwind-merge";
 
 type NextButtonProps = {
   text?: string;
   delay?: number;
   link: string;
+  textStyle?: string;
 };
-export default function NextButton({ text, delay, link }: NextButtonProps) {
+export default function NextButton({
+  text,
+  delay,
+  link,
+  textStyle,
+}: NextButtonProps) {
   const [hoverState, setHoverState] = useState(false);
 
   const options = {
@@ -25,11 +32,16 @@ export default function NextButton({ text, delay, link }: NextButtonProps) {
     <DivFadeUp delay={delay ? delay : 6}>
       <Link
         href={link}
-        className="text-primary font-semibold hover:brightness-110 duration-200 ease-in-out"
+        className="text-primary font-semibold hover:brightness-110 brightness-90 duration-200 ease-in-out"
         onMouseOver={() => setHoverState(true)}
         onMouseLeave={() => setHoverState(false)}
       >
-        <span className="flex gap-2 items-center justify-center">
+        <span
+          className={twMerge(
+            "flex gap-2 items-center justify-center",
+            textStyle
+          )}
+        >
           {text} <span>{arrow}</span>
         </span>
       </Link>
