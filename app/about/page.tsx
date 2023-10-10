6dc811d07e66d99228b93d7a05ee93a93cfdd286
ikeawesom <ikelimjl@gmail.com>
2023-10-11 00:10:54 +0800

    CTA to projects

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,8 @@ import PageWrapper from "@/src/components/PageWrapper";
 import Stack from "@/src/screens/about/Stack";
 import EducationSection from "@/src/screens/about/Education";
 import AwardsSection from "@/src/screens/about/Awards";
+import SectionContainer from "@/src/components/sections/SectionContainer";
+import NextButton from "@/src/components/utils/buttons/NextButton";
 
 export const metadata: Metadata = {
   title: "About",
@@ -19,6 +21,14 @@ export default function AboutPage() {
         <Stack />
         <EducationSection />
         <AwardsSection />
+        <SectionContainer className="py-8 pb-20">
+          <p className="text-dark-grey dark:text-light-grey">Impressed?</p>
+          <NextButton
+            text="Take a look at what I've done."
+            delay={2}
+            link="/portfolio"
+          />
+        </SectionContainer>
       </PageWrapper>
     </AnimationWrapper>
   );
